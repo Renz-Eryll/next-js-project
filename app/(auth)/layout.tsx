728808dc0e4c8ef7e4ae690001494c9410daad3b
Renz-Eryll <renzeryll09@gmail.com>
2025-06-16 23:46:23 +0800

    first commit

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,57 @@
+import React from "react";
+
+const Layout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <div className="flex min-h-screen">
+      <section className="bg-[var(--brand)] p-10 hidden w-1/2 items-center justify-center lg:flex xl:w-2/5">
+        <div className="flex max-h-[800px] max-w-[430px] flex-col justify-center space-y-12">
+          <div className="flex items-center gap-5">
+            <img
+              src="./assets/icons/favicon.ico"
+              alt="logo"
+              width={35}
+              height={35}
+              className="h-auto bg-white rounded-full p-1"
+            />
+            <div>
+              <h1 className="h2 text-white">Sync It</h1>
+            </div>
+          </div>
+          <div className="space-y-5 text-white">
+            <h1 className="h1 ">Manage your files the best way</h1>
+            <p className="body-1">
+              This is a place where you can store all your documents
+            </p>
+          </div>
+          <img
+            src="../assets/images/illustration.png"
+            alt="Illustration"
+            width={300}
+            height={300}
+            className="transition-all  hover:rotate-2 hover:scale-105"
+          />
+        </div>
+      </section>
+
+      <section className="flex flex-1 flex-col items-center bg-white p-4 py-10 lg:justify-center lg:p-10 lg:py-0">
+        <div className="mb-16 lg:hidden">
+          <div className="flex items-center gap-2">
+            <img
+              src="/favicon.ico"
+              alt="logo"
+              width={55}
+              height={55}
+              className="h-auto bg-white rounded-full p-1 w-[55px] lg:w-[65px]"
+            />
+            <div>
+              <h1 className="h2 text-[var(--brand)]">Sync It</h1>
+            </div>
+          </div>
+        </div>
+        {children}
+      </section>
+    </div>
+  );
+};
+
+export default Layout;
---
+++ components/AuthForm.tsx
@@ -0,0 +1,159 @@
+"use client";
+
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { useState } from "react";
+import Link from "next/link";
+
+const formSchema = z.object({
+  username: z.string().min(2).max(50),
+});
+
+type FormType = "sign-in" | "sign-up";
+
+const authFormSchema = (formType: FormType) => {
+  return z.object({
+    email: z.string().email(),
+    fullName:
+      formType === "sign-up"
+        ? z.string().min(2).max(50)
+        : z.string().optional(),
+  });
+};
+
+const AuthForm = ({ type }: { type: FormType }) => {
+  const [isLoading, setLoading] = useState(false);
+  const [errorMessage, setErrorMessage] = useState("");
+  const formSchema = authFormSchema(type);
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      fullName: "",
+      email: "",
+    },
+  });
+
+  const onSubmit = async (values: z.infer<typeof formSchema>) => {
+    console.log(values);
+  };
+
+  return (
+    <>
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className="auth-form">
+          <h1 className="form-title h1 text-center text-light-100 md:text-left">
+            {type === "sign-in" ? "Sign In" : "Sign Up"}
+          </h1>
+          {type === "sign-up" && (
+            <FormField
+              control={form.control}
+              name="fullName"
+              render={({ field }) => (
+                <FormItem>
+                  <div className="shad-form-item">
+                    <FormLabel className="shad-form-label">Full Name</FormLabel>
+                    <FormControl>
+                      <Input
+                        placeholder="Enter your full name"
+                        className="shad-input"
+                        {...field}
+                      />
+                    </FormControl>
+                  </div>
+                  <FormMessage className="shad-form-message" />
+                </FormItem>
+              )}
+            />
+          )}
+          <FormField
+            control={form.control}
+            name="email"
+            render={({ field }) => (
+              <FormItem>
+                <div className="shad-form-item">
+                  <FormLabel className="shad-form-label">Email</FormLabel>
+                  <FormControl>
+                    <Input
+                      placeholder="Enter your email"
+                      className="shad-input"
+                      {...field}
+                    />
+                  </FormControl>
+                </div>
+                <FormMessage className="shad-form-message" />
+              </FormItem>
+            )}
+          />
+          <Button
+            type="submit"
+            className="form-submit-button"
+            disabled={isLoading}
+          >
+            {type === "sign-in" ? "Sign In " : "Sign Up"}
+            {isLoading && (
+              <svg
+                aria-hidden="true"
+                className="animate-spin h-24 w-24 ml-2 text-gray-200 fill-[var(--brand)]"
+                viewBox="0 0 100 101"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  d="M100 50.5C100 78.3 77.6 100.5 50 
+      100.5C22.4 100.5 0 78.3 0 50.5C0 22.7 
+      22.4 0.5 50 0.5C77.6 0.5 100 22.7 100 
+      50.5ZM9.1 50.5C9.1 73.5 27 91.4 50 
+      91.4C73 91.4 90.9 73.5 90.9 50.5C90.9 
+      27.5 73 9.6 50 9.6C27 9.6 9.1 27.5 9.1 
+      50.5Z"
+                  fill="currentColor"
+                />
+                <path
+                  d="M93.9 39.0C96.8 38.3 98.5 35.2 
+      97.4 32.4C95.5 27.7 92.9 23.3 
+      89.5 19.4C85.5 14.9 80.6 11.3 
+      75.1 8.8C69.6 6.3 63.6 5 57.5 
+      5C54.4 5 52 7.4 52 10.5C52 13.6 
+      54.4 16 57.5 16C61.8 16 66 16.9 
+      69.8 18.7C73.6 20.5 77 23.1 79.7 
+      26.4C81.8 28.9 83.5 31.8 84.7 
+      35C85.7 37.8 91.1 39.7 93.9 39Z"
+                  fill="currentFill"
+                />
+              </svg>
+            )}
+          </Button>
+          {errorMessage && <p className="error-message">*{errorMessage}</p>}
+
+          <div className="body-2 flex justify-center">
+            <p className="text-[var(--light-100)]">
+              {type === "sign-in"
+                ? "Don't have an account?"
+                : "Already have an account?"}
+            </p>
+            <Link
+              href={type === "sign-in" ? "/sign-up" : "/sign-in"}
+              className="ml-1 font-medium text-[var(--brand)]"
+            >
+              {type === "sign-in" ? "Sign Up" : "Sign In"}
+            </Link>
+          </div>
+        </form>
+      </Form>
+      {/* OTP Verification */}
+    </>
+  );
+};
+
+export default AuthForm;
