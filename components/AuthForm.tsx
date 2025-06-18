e903deddcfe1d899be0a0f7c77de845b0ff8a5a3
Renz-Eryll <renzeryll09@gmail.com>
2025-06-18 21:24:15 +0800

    OTP modal

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -16,6 +16,7 @@ import { Input } from "@/components/ui/input";
 import { useState } from "react";
 import Link from "next/link";
 import { createAccount } from "@/lib/actions/user.actions";
+import OTPModal from "./OTPModal";
 
 type FormType = "sign-in" | "sign-up";
 
@@ -115,36 +116,13 @@ const AuthForm = ({ type }: { type: FormType }) => {
           >
             {type === "sign-in" ? "Sign In " : "Sign Up"}
             {isLoading && (
-              <svg
-                aria-hidden="true"
-                className="animate-spin h-24 w-24 ml-2 text-gray-200 fill-[var(--brand)]"
-                viewBox="0 0 100 101"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  d="M100 50.5C100 78.3 77.6 100.5 50 
-      100.5C22.4 100.5 0 78.3 0 50.5C0 22.7 
-      22.4 0.5 50 0.5C77.6 0.5 100 22.7 100 
-      50.5ZM9.1 50.5C9.1 73.5 27 91.4 50 
-      91.4C73 91.4 90.9 73.5 90.9 50.5C90.9 
-      27.5 73 9.6 50 9.6C27 9.6 9.1 27.5 9.1 
-      50.5Z"
-                  fill="currentColor"
-                />
-                <path
-                  d="M93.9 39.0C96.8 38.3 98.5 35.2 
-      97.4 32.4C95.5 27.7 92.9 23.3 
-      89.5 19.4C85.5 14.9 80.6 11.3 
-      75.1 8.8C69.6 6.3 63.6 5 57.5 
-      5C54.4 5 52 7.4 52 10.5C52 13.6 
-      54.4 16 57.5 16C61.8 16 66 16.9 
-      69.8 18.7C73.6 20.5 77 23.1 79.7 
-      26.4C81.8 28.9 83.5 31.8 84.7 
-      35C85.7 37.8 91.1 39.7 93.9 39Z"
-                  fill="currentFill"
-                />
-              </svg>
+              <img
+                src="/assets/icons/loader.svg"
+                alt="loader"
+                width={24}
+                height={24}
+                className="ml-2 animate-spin"
+              />
             )}
           </Button>
           {errorMessage && <p className="error-message">*{errorMessage}</p>}
@@ -165,6 +143,10 @@ const AuthForm = ({ type }: { type: FormType }) => {
         </form>
       </Form>
       {/* OTP Verification */}
+
+      {accountId && (
+        <OTPModal email={form.getValues("email")} accountId={accountId} />
+      )}
     </>
   );
 };
